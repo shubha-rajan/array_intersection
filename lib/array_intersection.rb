3fddb9805af29f52a11e0c5cf90c365bf77e68a3
Shubha Rajan <shubharajan94@gmail.com>
2019-04-15 17:45:43 -0700

    implemented hash table

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,22 +1,22 @@
 # Returns a new array to that contains elements in the intersection of the two input arrays
-# Time complexity: O(n * m) where n is the length of array1 and m is the length of array 2 because in a worst case scenario you would have to iterate through the entire second array for each element in the first array
-# Space complexity: n, if n < m; If every element in the smaller array overlaps, the size of the new array will equal the size of the smaller array.
+# Time complexity: O(n + m) where n is the length of array1 and m is the length of array 2 because you would have to iterate through the entire first array to build the hash table, and the entire second array to check if values are in the hash table
+# Space complexity: n, if n < m; If every element in the smaller array overlaps, the size of the new array will equal the size of the smaller array. The size of the hash table is also proportional to the size of the array.
 
 def intersection(array1, array2)
-  i = 0
-  j = 0
+  hash_table = {}
   result = []
   if array1 && array2
+    i = 0
     while i < array1.length
-      while j < array2.length
-        if array1[i] == array2[j]
-          result << array1[i]
-          break
-        end
-        j += 1
+      hash_table[array1[i]] = 1
+      i += 1
+    end
+    i = 0
+    while i < array2.length
+      if hash_table[array2[i]]
+        result << array2[i]
       end
       i += 1
-      j = 0
     end
   end
   return result
